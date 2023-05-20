f29b26d8e4d9c647442af2620d1d4dced9614488
Rupa polisetti <112394661+Rupa-suresh@users.noreply.github.com>
2023-05-20 23:49:46 +0530

    stringmethods

---
+++ assessment/stringmethods.js
@@ -0,0 +1,29 @@
+const str = "The Phoenix Guild is the best Women in Web3 Community";
+console.log("Length:", str.length);
+
+console.log("Uppercase:", str.toUpperCase());
+
+console.log("Lowercase:", str.toLowerCase());
+
+console.log("Index of 'Guild':", str.indexOf("Guild"));
+
+console.log("Last Index of 'he':", str.lastIndexOf("he"));
+
+console.log("Includes 'Women':", str.includes("Women"));
+
+console.log("Starts with 'The':", str.startsWith("The"));
+
+console.log("Ends with 'Community':", str.endsWith("Community"));
+
+console.log("Slice (8-15):", str.slice(8, 15));
+
+console.log("Substring (4-11):", str.substring(4, 11));
+
+console.log("Replace 'best' with 'great':", str.replace("best", "great"));
+
+console.log("Trimmed:", str.trim());
+
+console.log("Character at index 6:", str.charAt(6));
+
+console.log("Split by space:", str.split(" "));
+
