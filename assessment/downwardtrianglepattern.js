41219afa714b2c798aba2261f4069a4895d2e095
Rupa polisetti <112394661+Rupa-suresh@users.noreply.github.com>
2023-06-03 00:20:53 +0530

    Update downwardtrianglepattern.js

--- assessment/downwardtrianglepattern.js
+++ assessment/downwardtrianglepattern.js
@@ -1,12 +1,10 @@
 function downwardTrianglePattern(row) {
     for (let i = row; i >= 1; i--) {
       let x = '';
-  
       for (let j = 1; j <= i; j++) {
         x += '*';
       }
-  
-      console.log(x);
+     console.log(x);
     }
   }
   downwardTrianglePattern(5);
